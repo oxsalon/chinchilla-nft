02f8b119332fff075bd2342f43417d6f0ddbdb14
chinchilla-official <chinchilla.marketing.2023@gmail.com>
2023-06-04 20:37:33 +0800

    feat: remove

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true,
-  images: {
-    loader: 'akamai',
-    path: ''
-  },
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' }
-    }
-  }
+  reactStrictMode: true
+  // trailingSlash: true,
+  // images: {
+  //   loader: 'akamai',
+  //   path: ''
+  // },
+  // exportPathMap: function () {
+  //   return {
+  //     '/': { page: '/' }
+  //   }
+  // }
 }
 
 module.exports = nextConfig
